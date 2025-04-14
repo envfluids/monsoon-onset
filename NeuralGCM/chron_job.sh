a3e1db80d2ce9bffd2b2a18faa1ec02276675557
amarchakitus <marchakitus@uchicago.edu>
2025-04-14 09:26:48 -0500

    Add chron_job.sh scripts for AIFS and NeuralGCM with job timeout handling

---
+++ NeuralGCM/chron_job.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+cd /scratch/midway3/marchakitus/neuralgcm/operational/utils
+
+source /home/marchakitus/.bashrc
+
+conda activate /home/marchakitus/.conda/envs/neuralgcm
+timeout 29m python ./pipeline.py
+
+if [ $? -eq 124 ]; then
+  echo "ERROR Job timed out after 29 minutes."
+fi
