4c07ea52c7fd88b34208f4d66d1e6bc43c6813d8
amarchakitus <marchakitus@uchicago.edu>
2025-05-07 09:42:06 -0500

    updated sync job timeout

--- sync/utils/cron_job.sh
+++ sync/utils/cron_job.sh
@@ -5,8 +5,8 @@ cd /project/pedramh/monsoon/monsoon-onset/sync/utils
 source /home/marchakitus/.bashrc
 
 conda activate /project/pedramh/monsoon/conda-envs/monsoon
-timeout 2m python ./main.py
+timeout 4m python ./main.py
 
 if [ $? -eq 124 ]; then
-  echo "ERROR:Job timed out after 2 minutes."
+  echo "ERROR:Job timed out after 4 minutes."
 fi
