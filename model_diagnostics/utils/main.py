02943a74312481dd1a2b29aee5887e1061eb9072
Adam Marchakitus <marchakitus@uchicago.edu>
2026-05-19 08:45:32 -0500

    added entrypoint to model diagnostics

--- model_diagnostics/utils/main.py
+++ model_diagnostics/utils/main.py
@@ -4,20 +4,29 @@ from argparse import ArgumentParser
 from india.circulation_main import plot_circulation as plot_circulation_india
 from india.plot_precip import plot_precip as plot_precip_india
 
+# from ethiopia.circulation_main import plot_circulation as plot_circulation_ethiopia 
+
 def main():
     parser = ArgumentParser(description="Run model diagnostics utilities.")
     parser.add_argument("--date", type=str, required=False, help="Initialization date in YYYYMMDDTHH format (e.g., 20250508T06).")
     parser.add_argument("--region", type=str, required=False, help="Region for which to run diagnostics.")
+    parser.add_argument("--model", type=str, required=False, help="Model for which to run diagnostics, only used for Ethiopia", choices=["AIFS", "AIFS_ENS"])
     args = parser.parse_args()
     date = args.date
     region = args.region
+    model = args.model
 
     base = Path(__file__).resolve().parent.parent.parent
     print(f"Base path: {base}")
     print(f"Date: {date}")
+    print(f"Region: {region}")
+    # print(f"Model: {model}")
     if region == "india":
         plot_circulation_india(base, date)
-        plot_precip_india(base, date)
+        plot_precip_india(base, date, model)
+    if region == "ethiopia":
+        # plot_circulation_ethiopia(base, date, model)
+        pass
     else:
         print(f"Region '{region}' not recognized. No diagnostics will be run.")
 
